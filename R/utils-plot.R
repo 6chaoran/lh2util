7dbf935f1650891d80921a41a68d55a7f240a011
Chaoran Liu <6chaoran@gmail.com>
2020-10-02 16:49:03 +0800

    Update utils-plot.R

--- R/utils-plot.R
+++ R/utils-plot.R
@@ -98,3 +98,352 @@ util.plot.stack.bar <- function(df, x, fill,
     labs(x = x, fill = fill, y = '') +
     scale_y_continuous(labels = scales::percent)
 }
+
+#' util.corr.compute
+#' 
+#' compute pairwise correlation with p-value
+#' 
+#' @importFrom tidyselect one_of
+#' @importFrom dplyr mutate_if bind_rows select pull arrange
+#' @importFrom progress progress_bar
+#' @importFrom stats cor.test
+#' @param data input data.frame
+#' @param fnames variables to perform correlation computation
+#' @return data.frame
+#' @export
+#' @examples 
+#' util.corr.compute(mtcars, c('am','gear','mpg'))
+util.corr.compute <- function(data, fnames){
+  # convert categorical to integer (big assumption)
+  data <- data %>%
+    select(one_of(fnames)) %>%
+    mutate_if(is.character, function(i) as.integer(factor(i))) %>%
+    mutate_if(is.factor, as.integer)
+
+  # only implement Pearson test
+  total <- length(fnames) * (length(fnames) - 1) / 2
+  pb <- progress_bar$new(
+    format = "  processing :what [:bar] :percent eta: :eta",
+    clear = FALSE,
+    width = 60,
+    total = total)
+
+  out <- list()
+  for(i in fnames){
+    for(j in fnames){
+      if(i > j){
+        pb$tick(tokens = list(what = glue("{i}-{j}   ")))
+        cor.ij <- cor.test(data %>% pull(i),
+                           data %>% pull(j),
+                           use = 'complete.obs')
+        row <- data.frame(var1 = i,
+                          var2 = j,
+                          corr = cor.ij$estimate,
+                          p.value = cor.ij$p.value)
+        out <- append(out, list(row))
+      }
+
+    }
+  }
+
+  out <- bind_rows(out) %>%
+    mutate(corr = round(corr,2),
+           p.value = round(p.value, 2))
+
+  out.diag <- data.frame(
+    var1 = fnames,
+    var2 = fnames,
+    corr = 1,
+    p.value = 0
+  )
+
+  return(out %>% 
+           bind_rows(out.diag) %>%
+           arrange(var1, var2))
+
+}
+
+#' util.corr.plot
+#' 
+#' plot pairwise correlation, used together with `util.corr.compute`
+#' 
+#' @param out data.frame output from `util.corr.compute`
+#' @param title title of the chart
+#' @importFrom ggplot2 labs aes
+#' @importFrom ggplot2 geom_text geom_tile scale_fill_gradient2
+#' @return plot
+#' @export
+#' @examples 
+#' out <- util.corr.compute(mtcars, colnames(mtcars))
+#' util.corr.plot(out)
+util.corr.plot <- function(out, title = 'Pairwise Correlation Plot'){
+  out %>%
+    mutate(p = ifelse(p.value < 0.05, '*', ''),
+           label = glue('{corr}{p}'),
+           label = ifelse(var1 == var2, '', label)) %>%
+    ggplot(aes(x = var2, y = var1, fill = corr, label = label)) +
+    geom_tile(color = 'red') +
+    geom_text(size = 3) +
+    scale_fill_gradient2(high = util.lh2.fill()) +
+    labs(fill = 'correlation',
+         title = title,
+         subtitle = '* indicates correlation signifcance',
+         x = '',
+         y = '') +
+    util.lh2.theme(font.size = 10)
+}
+
+
+#' util.profile.num
+#' 
+#' profile the numeric variable by segment, used for segment profiling
+#' 
+#' @param data input data.frame
+#' @param x numeric variable for profiling
+#' @param segment variable name of the segment
+#' @param alpha float density plot transparency
+#' @param scale float scale of density plot
+#' @return list(data.frame, plot)
+#' @export
+#' @importFrom dplyr rename filter
+#' @importFrom ggridges geom_density_ridges_gradient
+#' @importFrom ggplot2 coord_cartesian
+#' @importFrom forcats fct_rev
+#' @examples 
+#' res <- util.profile.num(mtcars, x = 'wt', segment = 'am')
+#' res$data
+#' res$plot
+#' 
+util.profile.num <- function(data, x, segment = 'segment',
+                             alpha = 0.8, scale = 1){
+  
+  data <- data %>%
+    rename(x = !!rlang::sym(x),
+           segment = !!rlang::sym(segment))
+
+  plot <- data %>%
+    filter(!is.na(x)) %>%
+    filter(x <= quantile(x, 0.99, na.rm = T)) %>%
+    filter(x >= quantile(x, 0.01, na.rm = T)) %>%
+    ggplot(aes(x = x, y = fct_rev(factor(segment)), fill = factor(segment))) +
+    geom_density_ridges_gradient(scale = scale, 
+                                 alpha = alpha,
+                                 quantile_lines = TRUE) +
+    labs(x = x, y = 'Segment', fill = 'Segment') +
+    coord_cartesian(expand = F) +
+    util.lh2.theme()
+
+  data <- data %>%
+    group_by(segment) %>%
+    summarise(
+      count = n(),
+      min = min(x, na.rm = T),
+      Q1 = quantile(x, 0.25, na.rm = T),
+      median = median(x, 0.5, na.rm = T),
+      mean = mean(x, na.rm = T),
+      Q3 = quantile(x, 0.75, na.rm = T),
+      max = max(x, na.rm = T)
+    )
+
+  return(list(data = data, plot = plot))
+
+}
+
+#' util.profile.cat
+#' 
+#' profile the numeric variable by segment, used for segment profiling
+#' 
+#' @inheritParams util.profile.num
+#' @param x char, categorical variable name
+#' @return list(data, plot)
+#' @export
+#' @importFrom ggplot2 coord_flip
+#' @importFrom tidyr pivot_wider
+#' @examples 
+#' res <- util.profile.cat(mtcars, x = 'am', segment = 'gear')
+#' res$data
+#' res$plot
+
+util.profile.cat <- function(data, x, segment = 'segment', 
+                             alpha = 0.8){
+
+  data <- data %>%
+    rename(x = !!rlang::sym(x),
+           segment = !!rlang::sym(segment))
+  res <-  data %>%
+    filter(!is.na(x) & x != '') %>%
+    mutate(x = factor(x),
+           segment = as.character(segment)) %>%
+    group_by(segment,x) %>%
+    summarise(n = n()) %>%
+    ungroup() %>%
+    group_by(segment) %>%
+    mutate(pct = n / sum(n),
+           start = cumsum(pct),
+           end = cumsum(lag(pct, 1, 0)),
+           position = 1 - (start + end) /2 ,
+           label = ifelse(pct > 0.1, scales::percent(pct, 1), '')) %>%
+    ungroup()
+
+  plot <- res %>%
+    ggplot(aes(x = fct_rev(factor(segment)), y = pct, fill = x)) +
+    geom_bar(stat = 'identity', position = 'stack', alpha = alpha) +
+    geom_text(aes(y = position, label = label)) +
+    labs(fill = x, x='Segment', y = '% of users') +
+    scale_y_continuous(labels = scales::percent) +
+    coord_flip(expand = F) +
+    util.lh2.theme()
+
+  total <- res %>%
+    group_by(x) %>%
+    summarise(n = sum(n)) %>%
+    mutate(segment = 'overall',
+           pct = n / sum(n)) %>%
+    ungroup()
+
+  data <- res %>%
+    select(segment, x, n, pct) %>%
+    bind_rows(total) %>%
+    mutate(pct = scales::percent(pct, 1),
+           n = format(n, big.mark = ','),
+           value = glue("{n} ({pct})")) %>%
+    rename(!!rlang::sym(x):= x) %>%
+    pivot_wider(!!rlang::sym(x),
+                names_from = segment,
+                values_from = value)
+
+  list(data = data, plot = plot)
+
+}
+
+#' util.profile.fnames
+#' 
+#' profile both numeric and categorical varaibles
+#' 
+#' @inheritParams util.profile.num
+#' @param data input data.frame
+#' @param fnames variables to be profiled
+#' @return list(data, plot)
+#' @export
+#' @examples 
+#' mtcars['am'] <- factor(mtcars[['am']])
+#' res <- util.profile.fnames(mtcars, c('am','wt'), segment = 'gear')
+#' library(ggpubr)
+#' ggarrange(plotlist = res$plot, ncol = 2 , nrow = 1, common.legend = TRUE)
+
+util.profile.fnames <- function(data, fnames, segment,
+                                alpha = 0.8, 
+                                scale = 1){
+
+  profiles.data <- list()
+  profiles.plot <- list()
+
+  pb <- progress_bar$new(
+    format = "  processing :what [:bar] :percent eta: :eta",
+    clear = FALSE,
+    width = 60,
+    total = length(fnames))
+
+  for(fname in fnames){
+
+    pb$tick(tokens = list(what = glue("{fname}   ")))
+
+    if(is.numeric(data[[fname]])){
+      res <- util.profile.num(data, fname, segment)
+      profiles.data[[fname]] <- res$data
+      profiles.plot[[fname]] <- res$plot
+    }
+
+    if(is.character(data[[fname]]) |
+       is.factor(data[[fname]])){
+      res <- util.profile.cat(data, fname, segment)
+      profiles.data[[fname]] <- res$data
+      profiles.plot[[fname]] <- res$plot
+    }
+  }
+
+  return(list(data = profiles.data, plot = profiles.plot))
+}
+
+#' util.segment.summarize
+#' 
+#' summarize segments in tables
+#' 
+#' @param data output from `util.profile.fnames`
+#' @param segment char, segment variable name
+#' @param summ.func segment summarize function `e.g function(i) summarise(i, n = n())`
+#' @param highlight.tol tolerance for highlighting
+#' @param kable boolean, TRUE -> return kable table
+#' @param ... extra params from `util.vis.kable`
+#' @export
+#' @importFrom dplyr n case_when
+#' @importFrom kableExtra cell_spec
+#' @examples 
+#' util.segment.summarize(mtcars, segment = 'gear')
+#' library(dplyr)
+#' summ.func <- function(i){
+#'   summarise(i, n = n(), 
+#'                `am-0` = mean(am == 0),
+#'                `am-1` = mean(am == 1))
+#' }
+#' util.segment.summarize(mtcars, 'gear', summ.func)
+util.segment.summarize <- function(data,
+                                   segment,
+                                   summ.func = NULL,
+                                   highlight.tol = 0.2,
+                                   kable = TRUE,
+                                   ...) {
+  
+  # highlight.tol:
+  # beyond the percentage of difference from the global will be highlighted
+  # segment.summ.func:
+  # custom function used for summarization
+  
+  data <- data %>%
+    rename(segment = !!rlang::sym(segment))
+
+  if(is.null(summ.func)) summ.func <- function(i) summarise(i, n = n())
+  # summarize segments
+  stat.segment <- data %>%
+    group_by(segment = as.character(segment)) %>%
+    summ.func
+
+  # summarize global
+  stat.global <- data %>%
+    summ.func %>%
+    mutate(segment = 'Global')
+
+  # bind rows of two stat. tables
+  res <- stat.segment %>%
+    mutate(segment = as.character(glue('Segment-{segment}'))) %>%
+    bind_rows(stat.global)
+
+  if ('n' %in% colnames(res)) {
+    res <- res %>%
+      mutate(n = format(n, big.mark = ',')) %>%
+      rename(`# of user` = n)
+  }
+  
+  if (kable) {
+    res %>%
+      mutate_if(is.numeric, function(i) {
+        cell_spec(
+          x = case_when(
+            all(between(i, 0, 1)) ~ scales::percent(i, 1),
+            i >= 1000 ~ format(as.integer(i), big.mark = ","),
+            TRUE ~ as.character(round(i, 0))
+          ),
+          color = case_when(
+            i > tail(i, 1) * (1 + highlight.tol) ~ 'red',
+            i < tail(i, 1) * (1 - highlight.tol) ~ 'blue',
+            i == tail(i, 1) ~ 'black',
+            TRUE ~ 'gray'
+          )
+        )
+      }) %>%
+      util.vis.kable(...)
+  } else {
+    res
+  }
+
+}
